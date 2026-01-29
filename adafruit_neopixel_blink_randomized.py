fd55f3539d6ae5e8adfc4517e3f78873fcf5d2ec
3t1-1aN <147291550+3t1-1aN@users.noreply.github.com>
2026-01-29 13:22:28 -0800

    Enhance code readability with comments
    Added comments for better code understanding.

--- adafruit_neopixel_blink_randomized.py
+++ adafruit_neopixel_blink_randomized.py
@@ -1,15 +1,15 @@
 import time
 import board
 import neopixel
-import random
-color_value = (0,0,0)
+import random #allows for randomization
+color_value = (0,0,0) #set the default color value
 
-
+#initialize all the neopixels to the variable "pixels" for easy access
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
 while True:
-    pixels[random.randint(0, 9)]=color_value
+    pixels[random.randint(0, 9)]=color_value #gets a random neopixel and assigns the color value
     time.sleep(0.05)
-    pixels.fill((0, 0, 0))
-    color_value = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
+    pixels.fill((0, 0, 0)) #technically, turns all the neopixels off by turning them black
+    color_value = (random.randint(0, 255), random.randint(0,255), random.randint(0,255)) #randomize the next color in the range of 0-255
 
